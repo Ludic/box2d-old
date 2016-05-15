0ed54aff530838cd5737e9d8baf9c818520c0140
Derek Arner <doman412@gmail.com>
2016-05-14 23:46:56 -0700

    this is working

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 
 export {default as DebugDraw} from './components/debugDraw.js'
-export {Module as Box2D} from './components/box2d.js'
+import Module from './components/box2d.js'
+export {Module as Box2D, Module as default};
 
+console.log('hello from box2d main.js', Module);
 
-console.log(DebugDraw);
-console.log(Box2D);
+// world
+export {default as World} from './components/world'
+export {default as WorldConfig} from './components/worldConfig'
+
+// rube
+export {default as RubeLoader} from './components/rube/RubeLoader'
+export {default as RubeScene} from './components/rube/RubeScene'
+export {default as RubeAssetLoader} from './components/rube/rubeAssetLoader'
+
+// console.log(DebugDraw);
+// console.log(Box2D);
