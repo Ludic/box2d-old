e91b84599247b243f3837b8868e0f0cde324a54d
yintothayang <yintothayang@gmail.com>
2017-01-22 11:46:18 -0500

    add webpack conf

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ export {default as DebugDraw} from './components/debugDraw.js'
    either be and object containing the Box2D lib or the lib itself.
 */
 import Box2D, * as __Box2D from './components/box2d.js'
-let _Box2D = window.Box2D || Box2D.Box2D || Box2D || ()=>{console.warn("Unknown config of Box2D. Failed to load library");return {};};
+let _Box2D = window.Box2D || Box2D.Box2D || Box2D; /* || ()=>{console.warn("Unknown config of Box2D. Failed to load library");return {};}; */
 export {_Box2D as Box2D, _Box2D as default};
 
 // world
